761cec13691cc65b8cb27de0f8d41debb45b3471
sharks619 <74135454+sharks619@users.noreply.github.com>
2024-09-30 01:46:37 +0900

    Title: 색깔 트리, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240930/색깔 트리/color-tree.py
+++ 240930/색깔 트리/color-tree.py
@@ -5,10 +5,9 @@ def addnodes(nodes_info_, abs_node_num, id_, p_id_, c_, dmax_):
     else:
         parent_depth = next((depth for _, depth, node_id, _, _, _ in nodes_info_ if node_id == p_id_), 0)
         node_info_ = [abs_node_num, parent_depth + 1, id_, p_id_, c_, dmax_]
-
+    
     nodes_info_.append(node_info_)
     return nodes_info_
-
 
 def change_color(nodes_info_, id_child_id, pid_, color_):
     # 자식 노드 리스트에 대해 색상을 한 번에 변경
@@ -17,26 +16,25 @@ def change_color(nodes_info_, id_child_id, pid_, color_):
             nodes_info_[n][4] = color_  # 색상만 변경
     return nodes_info_
 
-
 def calculate_dict_id_child_id(nodes_info_):
     id_child_id = {id_: set([id_]) for _, _, id_, _, _, _ in nodes_info_}
 
-    for _, _, id_, pid_, _, _ in nodes_info_:
+    # 부모-자식 관계를 정확하게 관리
+    for _, _, id_, pid_, _, _ in sorted(nodes_info_, key=lambda x: x[1], reverse=True):
         if pid_ != -1:
             id_child_id[pid_].update(id_child_id[id_])
 
     return id_child_id
 
-
 def calculate_dict_id_child_color(nodes_info_):
     id_child_color = {id_: set([color]) for _, _, id_, _, color, _ in nodes_info_}
 
-    for _, _, id_, pid_, color, _ in nodes_info_:
+    # 부모-자식 관계를 정확하게 관리
+    for _, _, id_, pid_, color, _ in sorted(nodes_info_, key=lambda x: x[1], reverse=True):
         if pid_ != -1:
             id_child_color[pid_].update(id_child_color[id_])
 
     return id_child_color
-
 
 def calculate_score(id_child_color):
     score = sum(len(colors) ** 2 for colors in id_child_color.values())
@@ -51,17 +49,4 @@ for x in range(q):
 
     if cmds == 100:
         p_id = args[1]
-        nodes_info = addnodes(nodes_info, x, *args)
-
-    elif cmds == 200:
-        id_child_id = calculate_dict_id_child_id(nodes_info)
-        nodes_info = change_color(nodes_info, id_child_id, *args)
-
-    elif cmds == 300:
-        color = next((c for _, _, id_, _, c, _ in nodes_info if id_ == args[0]), None)
-        print(color)
-
-    elif cmds == 400:
-        id_child_color = calculate_dict_id_child_color(nodes_info)
-        score = calculate_score(id_child_color)
-        print(score)
+        nodes_info = addnodes(nodes_info, x, *args
