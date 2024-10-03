9f3eb70158efe5d0f03b56de4bb460bc8d91c0e2
sharks619 <74135454+sharks619@users.noreply.github.com>
2024-10-03 20:12:14 +0900

    Title: 토끼와 경주, Time: 8453ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 241003/토끼와 경주/rabit-and-race.py
+++ 241003/토끼와 경주/rabit-and-race.py
@@ -1,11 +1,9 @@
 import heapq
 from collections import defaultdict
 
-dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 rabbits = []
-heapq.heapify(rabbits)
-
 score_dic = defaultdict(int)
 dist_dic = defaultdict(int)
 
@@ -13,72 +11,97 @@ q = int(input())
 _, n, m, p, *args = list(map(int, input().split()))
 
 for i in range(p):
-    id, d = args[i*2:i*2+2]
-    heapq.heappush(rabbits,(0, 0, 0, id))  # (cnt, r + c, r, id)
+    id, d = args[i * 2:i * 2 + 2]
+    heapq.heappush(rabbits, (0, 0, 0, id))  # (cnt, r + c, r, id)
     score_dic[id] = 0
     dist_dic[id] = d
 
 
-def race(k,s):
-    for _ in range(k):
-        jump_c, r_c, r, id = heapq.heappop(rabbits)
+def race(k, s):
+    selected_rabbits = []
+
+    # K번의 턴을 처리
+    for KK in range(k):
+        jump_c, c_rc, c_r, id = heapq.heappop(rabbits)
         jump_c += 1
-        r, c = r, r_c - r
+        cr, cc = c_r, c_rc - c_r
         d = dist_dic[id]
         sub = []
-
+        # print(KK+1)
+        # print('점프 전 id,r,c', id,cr,cc)
         for i in range(4):
             dist = d
-            if i==0: # r 그대로
-                dist %= 2*(m-1)
-                nr, nc = r, c + dist
-                if nc > m-1:
-                    nc = (m-1)-(dist-m+c+1)
+            r,c = cr,cc
+            if i == 0:  # 오른쪽 이동
+                dist %= 2 * (m - 1)
+                nc = c + dist
+                if nc > m - 1:
+                    nc = (m - 1) - (dist - (m - c - 1))
+                    if nc < 0:
+                        nc = abs(nc)
+                sub.append((r + nc, r, nc))
+                # print(r, nc)
 
-            elif i==1: # c 그대로
-                dist %= 2*(n-1)
-                nr, nc = r + dist, c
-                if nr > n-1:
-                    nr = (n-1)-(dist-n+r+1)
+            elif i == 1:  # 아래쪽 이동
+                dist %= 2 * (n - 1)
+                nr = r + dist
+                if nr > n - 1:
+                    nr = (n - 1) - (dist - (n - r - 1))
+                    if nr < 0:
+                        nr = abs(nr)
+                sub.append((nr + c, nr, c))
+                # print(nr, c)
 
-            elif i==2: # r 그대로
-                dist %= 2*(m-1)
-                nr, nc = r, c - dist
+            elif i == 2:  # 왼쪽 이동
+                dist %= 2 * (m - 1)
+                nc = c - dist
                 if nc < 0:
-                    nc = dist-c
+                    nc = dist - c
+                    if nc > m - 1:
+                        over = nc - (m - 1)
+                        nc = m - 1 - over
+                sub.append((r + nc, r, nc))
+                # print(r, nc)
 
-            elif i==3: # c 그대로
-                dist %= 2*(n-1)
-                nr, nc = r - dist, c
-                if nr < 0 :
-                    nr = dist-r
+            elif i == 3:  # 위쪽 이동
+                dist %= 2 * (n - 1)
+                nr = r - dist
+                if nr < 0:
+                    nr = dist - r
+                    if nc > n - 1:
+                        over = nc - (n - 1)
+                        nc = n - 1 - over
+                sub.append((nr + c, nr, c))
+                # print(nr, c)
 
-            sub.append((nr+nc, nr, nc))
+        # 가장 우선순위 높은 위치로 이동
+        _, r_, c_ = sorted(sub, key=lambda x: (-x[0], -x[1], -x[2]))[0]
+        # print('점프 후 id,r,c',id,r_,c_)
 
-        _, r, c = sorted(sub, key=lambda x: (-x[0],-x[1],-x[2]))[0]
-        heapq.heappush(rabbits,(jump_c, r+c, r, id))  # (cnt, r + c, r, id)
+        # 토끼를 다시 힙에 넣고, 선택된 토끼 리스트에 추가
+        heapq.heappush(rabbits, (jump_c, r_ + c_, r_, id))
+        selected_rabbits.append((jump_c, r_ + c_, r_, id))
 
+        # 모든 토끼의 점수를 갱신
         for key in score_dic.keys():
             if key != id:
-                score_dic[key] += r+c
+                score_dic[key] += r_ + c_ + 2
 
-    new_rabbits = rabbits[:]
-    new_rabbits_list = sorted(new_rabbits, key=lambda x: (-x[1],-x[2],-x[3]))
-    for i in new_rabbits_list:
-        if i[0]:
-            r_id = i[3]
-            score_dic[r_id] += s
+    # 선택된 토끼들 중에서 우선순위가 높은 토끼 선택
+    selected_rabbits = sorted(selected_rabbits, key=lambda x: (-x[1], -x[2], -x[3]))
+    r_id = selected_rabbits[0][3]
+
+    # 점수 S 추가
+    score_dic[r_id] += s
 
 
-for _ in range(q-1):
+for _ in range(q - 1):
     cmd, *args = list(map(int, input().split()))
     if cmd == 200:
         K, S = args
         race(K, S)
-
     elif cmd == 300:
         pid_t, L = args
         dist_dic[pid_t] *= L
-
     elif cmd == 400:
         print(max(score_dic.values()))
