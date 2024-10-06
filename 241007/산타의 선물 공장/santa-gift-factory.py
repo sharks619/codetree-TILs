8f3a96f6f2bf42558fe17841a3dd5e51576f35c6
sharks619 <74135454+sharks619@users.noreply.github.com>
2024-10-07 04:34:46 +0900

    Title: 산타의 선물 공장, Time: 845ms, Memory: 61MB, Status: Wrong Answer - CodeTree

--- 241007/산타의 선물 공장/santa-gift-factory.py
+++ 241007/산타의 선물 공장/santa-gift-factory.py
@@ -1,97 +1,154 @@
-from collections import deque, defaultdict
+from collections import defaultdict
+
+max_m = 10
+weight = {}  # id:weight
+prv, nxt = defaultdict(lambda: 0), defaultdict(lambda: 0)  # 0은 없다는 뜻
+head = [0] * max_m
+tail = [0] * max_m
+broken = [False] * max_m
+belt_num = defaultdict(lambda: -1)  # id:belt num, -1이면 사라진 것
 
 q = int(input())
-
 _, n, m, *args = list(map(int, input().split()))
-num = n//m
-belt_q = [deque() for _ in range(m)]
-belt_s = [set() for _ in range(m)]
-belt_dic = defaultdict(bool)
+ids, ws = args[:n], args[n:]
 
+for i in range(n):
+    weight[ids[i]] = ws[i]
 
 for i in range(m):
-    belt_dic[i] = False
-    for j in range(i*(n//m), (i+1)*(n//m)):
-        belt_q[i].append((args[j],args[j+n]))
-        belt_s[i].add(args[j])
+    head[i] = ids[i * (n // m)]
+    tail[i] = ids[(i + 1) * (n // m) - 1]
+    for j in range(i * (n // m), (i + 1) * (n // m)):
+        belt_num[ids[j]] = i
+        if j < (i + 1) * (n // m) - 1:
+            nxt[ids[j]] = ids[j + 1]
+            prv[ids[j + 1]] = ids[j]
 
-def down(x):
+def box_down(x):
     ans = 0
     for i in range(m):
-        if not belt_dic[i]:  # q가 비어있지 않은 경우에만 pop 시도 (벨트 고장일 수도..!)
-            c_id, c_w = belt_q[i].popleft()
-            if c_w <= x:
-                ans += c_w
-                belt_s[i].remove(c_id)
+        if not broken[i]:
+            h_id = head[i]
+            if h_id == 0:  # 벨트가 비어있을 때 처리
+                continue
+            if weight[h_id] <= x:
+                ans += weight[h_id]
+                belt_num[h_id] = -1  # 벨트에서 제거
+                head[i] = nxt[h_id]  # head 업데이트
+                if head[i] != 0:
+                    prv[head[i]] = 0  # 새로운 head의 prv를 0으로 설정
+                else:
+                    tail[i] = 0  # 벨트가 비었으면 tail도 0으로 설정
+                nxt[h_id] = 0  # 제거된 상자의 nxt 초기화
             else:
-                belt_q[i].append((c_id,c_w))
+                # 맨 뒤로 보내기
+                tail[i] = h_id
+                head[i] = nxt[h_id]
+                nxt[tail[i]] = h_id
+                prv[h_id] = tail[i]
+                nxt[h_id] = 0
     return ans
 
-def remove_box(x):
-    for i in range(m):
-        for c_id,c_w in belt_q[i]:
-            if c_id==x:
-                belt_q[i].remove((c_id,c_w))
-                belt_s[i].remove(c_id)
-                return c_id
-    return -1
+def box_remove(x):
+    belt_n = belt_num[x]
 
-def check_box(x):
-    for i in range(m):
-        for idx, (c_id, c_w) in enumerate(belt_q[i]):
-            if c_id == x:
-                belt_q[i].rotate(-idx)
-                return i + 1
-    return -1
+    if belt_n == -1:
+        return -1
 
-def move_box(x):
-    nxt_num = 0
-    for i in range(1,m):
-        nxt_num = (x - 1 + i) % m
-        if not belt_dic[nxt_num]:
-            break # 찾으면 끝내기!!
+    h_id, t_id = head[belt_n], tail[belt_n]
+    p_id, n_id = prv[x], nxt[x]
 
-    belt_q[nxt_num].extend(belt_q[x-1])
-    belt_q[x-1] = deque()
+    if x == h_id:
+        head[belt_n] = n_id
+    elif x == t_id:
+        tail[belt_n] = p_id
 
-    belt_s[nxt_num].update(belt_s[x-1])
-    belt_s[x-1] = set()
+    if n_id != 0:
+        prv[n_id] = p_id
+    if p_id != 0:
+        nxt[p_id] = n_id
 
+    prv[x], nxt[x] = 0, 0
+    belt_num[x] = -1
 
-for _ in range(q-1):
+    return x
+
+def box_check(x):
+    belt_n = belt_num[x]
+
+    if belt_n == -1:
+        return -1
+
+    h_id = head[belt_n]
+
+    if x != h_id:
+        # 상자를 앞으로 보내기
+        p_id, n_id = prv[x], nxt[x]
+
+        if x == tail[belt_n]:
+            tail[belt_n] = p_id
+        if p_id != 0:
+            nxt[p_id] = n_id
+        if n_id != 0:
+            prv[n_id] = p_id
+
+        nxt[x] = h_id
+        prv[h_id] = x
+        head[belt_n] = x
+        prv[x] = 0
+
+    return belt_n + 1
+
+def box_move(x):
+    if broken[x - 1]:
+        return -1
+
+    broken[x - 1] = True
+
+    nxt_n = 0
+    for i in range(1, m):
+        nxt_n = (x - 1 + i) % m
+        if not broken[nxt_n]:
+            break
+
+    h_id, t_id = head[x - 1], tail[x - 1]
+
+    if h_id == 0:  # 고장난 벨트가 비어있는 경우
+        return x
+
+    while h_id != 0:
+        belt_num[h_id] = nxt_n
+        h_id = nxt[h_id]
+
+    # 벨트 병합
+    if head[nxt_n] == 0:
+        head[nxt_n] = head[x - 1]
+    else:
+        nxt[tail[nxt_n]] = head[x - 1]
+        prv[head[x - 1]] = tail[nxt_n]
+
+    tail[nxt_n] = tail[x - 1]
+
+    # 고장난 벨트 초기화
+    head[x - 1], tail[x - 1] = 0, 0
+
+    return x
+
+for _ in range(q - 1):
     cmd, *args = list(map(int, input().split()))
-
-    # print()
-    # print(cmd, *args)
-    # print("전")
-    # print("belt_dic", belt_dic)
-    # print("belt_q", belt_q)
-    # print("belt_s", belt_s)
-    # print()
 
     if cmd == 200:
         w_max = args[0]
-        print(down(w_max))
+        print(box_down(w_max))
 
     elif cmd == 300:
         r_id = args[0]
-        print(remove_box(r_id))
+        print(box_remove(r_id))
 
     elif cmd == 400:
         f_id = args[0]
-        print(check_box(f_id))
+        print(box_check(f_id))
 
     elif cmd == 500:
         b_num = args[0]
-        if belt_dic[b_num-1]:
-            print(-1)
-        else:
-            belt_dic[b_num-1] = True
-            move_box(b_num)
-            print(b_num)
-
-    # print()
-    # print("후")
-    # print("belt_dic", belt_dic)
-    # print("belt_q", belt_q)
-    # print("belt_s", belt_s)
+        print(box_move(b_num))
