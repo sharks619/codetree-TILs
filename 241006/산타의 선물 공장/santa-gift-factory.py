65d2a3a7824f91af153ff7f16b09cddce9374582
sharks619 <74135454+sharks619@users.noreply.github.com>
2024-10-06 23:43:18 +0900

    Title: 산타의 선물 공장, Time: 156ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241006/산타의 선물 공장/santa-gift-factory.py
+++ 241006/산타의 선물 공장/santa-gift-factory.py
@@ -12,82 +12,62 @@ belt_dic = defaultdict(bool)
 for i in range(m):
     belt_dic[i+1] = False
     for j in range(i*(n//m), (i+1)*(n//m)):
-        belt_q[i].append((args[j],args[j+n]))
+        belt_q[i].append((args[j], args[j+n]))
         belt_s[i].add(args[j])
+
 def down(x):
     ans = 0
     for i in range(m):
-        c_id, c_w = belt_q[i].popleft()
-        if c_w <= x:
-            ans += c_w
-            belt_s[i].remove(c_id)
-        else:
-            belt_q[i].append((c_id,c_w))
-            belt_s[i].add(c_id)
+        if belt_q[i]:  # q가 비어있지 않은 경우에만 pop 시도 (벨트 고장일 수도..!)
+            c_id, c_w = belt_q[i].popleft()
+            if c_w <= x:
+                ans += c_w
+                belt_s[i].remove(c_id)
+            else:
+                belt_q[i].appendleft((c_id, c_w))  # 다시 앞에 넣기
     return ans
 
 def remove_box(x):
-    ans = 0
-    flag = False
     for i in range(m):
-        for c_id,c_w in belt_q[i]:
-            if c_id==x:
-                belt_q[i].remove((c_id,c_w))
+        for c_id, c_w in belt_q[i]:
+            if c_id == x:
+                belt_q[i].remove((c_id, c_w))
                 belt_s[i].remove(c_id)
-                flag = True
-                ans = c_id
-                break
-        if flag:
-            break
-
-    if not flag:
-        return -1
-    return ans
+                return c_id
+    return -1
 
 def check_box(x):
-    ans = 0
-    flag = False
     for i in range(m):
         cnt = 0
-        for c_id,c_w in belt_q[i]:
+        for c_id, c_w in belt_q[i]:
             cnt += 1
-            if c_id==x:
-                belt_q[i].rotate(-1*cnt)
-                ans = i+1
-                flag = True
-                break
-        if flag:
-            break
-
-    if not flag:
-        return -1
-    else:
-        return ans
+            if c_id == x:
+                belt_q[i].rotate(-1 * cnt)
+                return i + 1
+    return -1
 
 def move_box(x):
-    nxt_num = 0
-    for i in range(1,m):
-        if not belt_dic[(x-1+i)%m]:
-            belt_dic[(x-1+i)%m] = True
-            nxt_num = (x-1+i)%m
+    nxt_num = -1
+    for i in range(1, m):
+        nxt_num = (x - 1 + i) % m
+        if not belt_dic[nxt_num + 1]:
+            belt_dic[nxt_num + 1] = True
+            break
 
-    belt_q[nxt_num].extend(belt_q[x-1])
-    belt_q[x-1] = deque()
+    if nxt_num != -1:
+        belt_q[nxt_num].extend(belt_q[x-1])
+        belt_q[x-1] = deque()
 
-    belt_s[nxt_num].update(belt_s[x-1])
-    belt_s[x-1] = set()
+        belt_s[nxt_num].update(belt_s[x-1])
+        belt_s[x-1] = set()
 
+        belt_dic[x] = False  # 벨트 이동 후 상태 초기화
+
+    return x
 
 
 for _ in range(q-1):
     cmd, *args = list(map(int, input().split()))
-
-    # print()
-    # print(cmd, *args)
-    # print("전")
-    # print("belt_q", belt_q)
-    # print("belt_s", belt_s)
-    # print()
 
     if cmd == 200:
         w_max = args[0]
@@ -103,12 +83,7 @@ for _ in range(q-1):
 
     elif cmd == 500:
         b_num = args[0]
-        if belt_dic[b_num-1]:
+        if belt_dic[b_num]:
             print(-1)
         else:
-            move_box(b_num)
-            print(b_num)
-
-    # print("후")
-    # print("belt_q", belt_q)
-    # print("belt_s", belt_s)
+            print(move_box(b_num))
