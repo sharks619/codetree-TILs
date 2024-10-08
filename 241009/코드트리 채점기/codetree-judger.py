361e8b3ec68c6c3aab2f78ecd6879c99e8b40c81
sharks619 <74135454+sharks619@users.noreply.github.com>
2024-10-09 00:52:40 +0900

    Title: 코드트리 채점기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241009/코드트리 채점기/codetree-judger.py
+++ 241009/코드트리 채점기/codetree-judger.py
@@ -1,136 +1,106 @@
 import sys
 import heapq
-from collections import OrderedDict, defaultdict
-from typing import Optional
+from collections import defaultdict
 
-class Task:
-    def __init__(self, t, p, url):
-        self.domain = url.split("/")[0]
-        self.id = url.split("/")[1]
-        self.p = p
-        self.t = t
+input = sys.stdin.readline()
+q = int(input())
 
-    def __lt__(self, other):
-        if self.p != other.p:
-            return self.p < other.p
-        return self.t < other.t
+waiting_q = [] # 채점 대기 큐에 있는 채점 task
+waiting_q_cnt = 0 # 채점 대기 큐에 있는 채점 task 수
+judging_dic = defaultdict()
 
-# 채점기 정보 {J_id: Task}
-J_dict = OrderedDict()
-# 대기 큐 Priority Queue (Task) dict
-domain_wait_q = defaultdict(list)
-# 대기 큐 안에 있는 url dict {url: T/F}
-wait_q_url_dict = defaultdict(bool)
-# 도메인 시간 정보 {domain: {start: int, end: int}}
-domain_time_dict = defaultdict(dict)
-# 현재 채점 중인 도메인 리스트 {domain: T/F}
-domain_judging_dict = defaultdict(bool)
+waiting_u_dic = defaultdict(bool)  # 채점 대기 큐에 있는 url
+judging_d_dic = defaultdict(bool)  # 채점 진행 큐에 있는 domain
+history_d_dic = {}  # 채점 끝난 큐의 domain 처리 시작, 종료 시간 기록
 
-def set_j(input_data):
-    """
-    Step 1
-    input_data: [N, u0]
-    채점기 세팅
-    u0 wait Q에 넣기
-    """
-    N = int(input_data[0])
-    u = input_data[1]
-    for i in range(1, N + 1):
-        J_dict[i] = None
-    heapq.heappush(domain_wait_q[u.split("/")[0]], Task(0, 1, u))
-    wait_q_url_dict[u] = True
+_, n, u = list(map(str, input().split()))
+n = int(n)
+domain, id = u.split('/')
+heapq.heappush(waiting_q, (1, 0, u))  # p, t, url
+waiting_u_dic[u] = True
+waiting_j_q = list(range(1, n+1)) # 채점기
+heapq.heapify(waiting_j_q)
+waiting_q_cnt += 1
 
-def insert_task_wait_q(input_data):
-    """
-    step 2
-    input_data = [t, p ,u]
-    waitQ에 새로운 task 넣기
-    """
-    t, p = map(int, input_data[:2])
-    u = input_data[2]
-    if not wait_q_url_dict[u]:
-        heapq.heappush(domain_wait_q[u.split("/")[0]], Task(t, p, u))
-        wait_q_url_dict[u] = True
 
-def pop_task_wait_q(t) -> Optional[Task]:
-    task_tmp_list = []
+for _ in range(q-1):
+    cmd, *args = list(map(str, input().split()))
+    cmd = int(cmd)
+    # print()
+    # print("cmd, *args:", cmd, *args)
+    # print('작업전')
+    # print("waiting_q:", waiting_q)
+    # print("waiting_q_cnt:", waiting_q_cnt)
+    # print("waiting_u_set:", waiting_u_set)
+    # print()
+    # print("judging_d_set:", judging_d_set)
+    # print("judging_dic:", judging_dic)
+    # print()
+    # print("history_d_dic:", history_d_dic)
+    # print()
+    if cmd == 200:
+        t, p, url = int(args[0]), int(args[1]), args[2]
 
-    for domain, pq in domain_wait_q.items():
-        if not domain_judging_dict[domain] and domain_wait_q[domain]:
-            curr_domain_time = domain_time_dict.get(domain, {"start": 0, "gap": 0})
-            if curr_domain_time["start"] + 3 * curr_domain_time["gap"] <= t:
-                heapq.heappush(task_tmp_list, heapq.heappop(domain_wait_q[domain]))
+        if not waiting_u_dic[url]:
+            heapq.heappush(waiting_q, (p, t, url))
+            waiting_u_dic[url] = True
+            waiting_q_cnt += 1
 
-    if task_tmp_list:
-        task = heapq.heappop(task_tmp_list)
-        for tmp in task_tmp_list:
-            heapq.heappush(domain_wait_q[tmp.domain], tmp)
-        return task
-    else:
-        return None
+    elif cmd == 300:
+        t = int(args[0])
 
-def try_task(input_data):
-    """
-    step 3
-    input_data = [t]
-    """
-    t = int(input_data[0])
+        if not waiting_j_q: # 쉬고 있는 채점기가 없다면 무시
+            continue
 
-    j_id = None
-    for i, v in J_dict.items():
-        if v is None:
-            j_id = i
-            break
+        temp_q = []
 
-    if j_id is not None:
-        task: Optional[Task] = pop_task_wait_q(t)
-        if task is not None:
-            J_dict[j_id] = task
-            domain_judging_dict[task.domain] = True
-            domain_time_dict[task.domain]["start"] = t
-            wait_q_url_dict[f"{task.domain}/{task.id}"] = False
+        while waiting_q:
+            cp, ct, cu = heapq.heappop(waiting_q)
+            c_domain, c_id = cu.split('/')
 
-def end_task(input_data):
-    """
-    step 4
-    input_data = [t J_id]
-    """
-    t, j_id = map(int, input_data)
-    if J_dict[j_id] is not None:
-        task = J_dict[j_id]
-        J_dict[j_id] = None
-        domain_judging_dict[task.domain] = False
-        domain_time_dict[task.domain]["gap"] = t - domain_time_dict[task.domain]["start"]
+            if judging_d_dic[c_domain]:
+                temp_q.append((cp, ct, cu))
+            elif (c_domain in history_d_dic) and (t < history_d_dic[c_domain][0] + 3 * (history_d_dic[c_domain][1] - history_d_dic[c_domain][0])):
+                temp_q.append((cp, ct, cu))
+            else:
+                jid = heapq.heappop(waiting_j_q)
+                judging_dic[jid] = (t, cu)
+                del waiting_u_dic[cu]
+                judging_d_dic[c_domain] = True
+                waiting_q_cnt -= 1
+                break
+        for item in temp_q:
+            heapq.heappush(waiting_q, item)
 
-def print_wait_q():
-    """
-    step5
-    """
-    answer = 0
-    for v in domain_wait_q.values():
-        answer += len(v)
-    print(answer)
+        if not temp_q:
+            waiting_q_cnt += len(temp_q)
 
-Q = int(sys.stdin.readline())
-for _ in range(Q):
-    query, *data = sys.stdin.readline().split()
+    elif cmd == 400:
+        t, j_id = int(args[0]), int(args[1])
 
-    # 100 N u0
-    if int(query) == 100:
-        set_j(data)
+        # 채점 중이던 작업이 없으면 무시
+        if j_id not in judging_dic:
+            continue
 
-    # 200 t p u
-    elif int(query) == 200:
-        insert_task_wait_q(data)
+        c_t, c_url = judging_dic[j_id]
+        c_domain = c_url.split('/')[0]
 
-    # 300 t
-    elif int(query) == 300:
-        try_task(data)
+        del judging_dic[j_id]
+        del judging_d_dic[c_domain]
 
-    # 400 t J_id
-    elif int(query) == 400:
-        end_task(data)
+        heapq.heappush(waiting_j_q, j_id)
+        history_d_dic[c_domain] = [c_t, t]
 
-    else:
-        # 500 t
-        print_wait_q()
+    elif cmd == 500:
+        print(waiting_q_cnt)
+
+    # print('작업후')
+    # print("waiting_q:", waiting_q)
+    # print("waiting_q_cnt:", waiting_q_cnt)
+    # print("waiting_u_set:", waiting_u_set)
+    # print()
+    # print("judging_d_set:", judging_d_set)
+    # print("judging_dic:", judging_dic)
+    # print()
+    # print("history_d_dic:", history_d_dic)
+    # print()
