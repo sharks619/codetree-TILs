c26cad2674f333596a56080f2133ba97798c5147
sharks619 <74135454+sharks619@users.noreply.github.com>
2024-10-10 01:29:52 +0900

    Title: 코드트리 오마카세, Time: 1127ms, Memory: 65MB, Status: Passed - CodeTree

--- 241010/코드트리 오마카세/codetree-omakase.py
+++ 241010/코드트리 오마카세/codetree-omakase.py
@@ -1,80 +1,62 @@
 from collections import defaultdict
 
-class Query:
-    def __init__(self, cmd, t, x, name, n):
-        self.cmd = cmd
-        self.t = t
-        self.x = x
-        self.name = name
-        self.n = n
-
-queries = [] # 명령들을 관리합니다.
-names = set() # 등장한 사람 목록을 관리합니다.
-p_queries = defaultdict(list) # 각 사람마다 주어진 초밥 명령만을 관리합니다.
-entry_time = {} # 각 사람마다 입장 시간을 관리합니다.
-position = {} # 각 손님의 위치를 관리합니다.
-exit_time = {} # 각 사람마다 퇴장 시간을 관리합니다.
-
 l, q = map(int, input().split())
+ccount, scount = 0, 0  # customer, sushi count
+customer = defaultdict(list)
+# name: [t, p, 남은 할당량] 저장
+sushi = defaultdict(dict)
+# name: {t: p, t: p} 저장
+
+# t초에 x위치에 앉은 손님이 먹을 수 있는 초밥: t-1초에 x-1위치의 초밥... t초~미래에는 x위치의 초밥 놓여지면 바로
+commands = []
+# 모든 명령 바로바로 실행하지 말고,
+# 초밥이 손님 온 뒤로 언제 먹혀지는지부터 알아낸 다음에, 명령 실행하며 명령 시점 비교하는 게 낫다
 for _ in range(q):
-    command = input().split()
-    cmd, t, x, n = -1, -1, -1, -1
-    name = ""
-    cmd = int(command[0])
-    if cmd == 100:
-        t, x, name = command[1:]
-        t, x = map(int, [t, x])
-    elif cmd == 200:
-        t, x, name, n = command[1:]
-        t, x, n = map(int, [t, x, n])
+    command, t, *detail = input().split()
+    if command == '100':  # 초밥 = name: {t: p, t: p} 저장
+        x, name = detail
+        t, x = int(t), int(x)
+        sushi[name][t] = x
+    elif command == '200':  # 손님 = name: [t, p, 남은 할당량] 저장
+        x, name, n = detail
+        t, x, n = int(t), int(x), int(n)
+        customer[name].append(t)
+        customer[name].append(x)
     else:
-        t = int(command[1])
+        t = int(t)
+    commands.append((command, t))
 
-    queries.append(Query(cmd, t, x, name, n))
+# 손님 = name: [t, p, 남은 할당량] 저장
+for name, val in customer.items():
+    ct, cp = val
+    exit_time = 0  # 손님이 exit하는 time은, 초밥이 들어온 st 순서대로가 아닌, 최종적으로 먹히게 되는 ft를 고려해야 함
+    for st in sushi[name]:
+        if st <= ct:  # 초밥 놓은 다음 사람 옴
+            np = (sushi[name][st] + (ct - st)) % l  # ct일 때, 초밥의 위치
+            # np -> cp 가려면 기다려야 하는 시간
+            wait = (cp - np) % l
+            # 회전하며 최종적으로 먹히는 시간
+            ft = wait + ct
+        else:  # 사람 온 다음에 초밥 놓음
+            np = sushi[name][st]
+            # sushi[name][st] -> cp 가려면 기다려야 하는 시간
+            wait = (cp - sushi[name][st]) % l
+            # 회전하며 최종적으로 먹히는 시간
+            ft = wait + st
+        commands.append(("111", ft ))  # ft에 del sushi[name][st] 초밥 제거
+        exit_time = max(exit_time, ft)
 
-    if cmd == 100:
-        p_queries[name].append(Query(cmd, t, x, name, n))
+    commands.append(("222", exit_time))  # 가장 마지막 ft에 손님 제거
 
-    elif cmd == 200:
-        names.add(name)
-        entry_time[name] = t
-        position[name] = x
-
-for name in names:
-    exit_time[name] = 0
-
-    for q in p_queries[name]:
-        # 초밥 먼저 등장
-        time_to_removed = 0
-        if q.t < entry_time[name]:
-            t_sushi_x = (q.x + (entry_time[name] - q.t)) % l
-            a_time = (position[name] - t_sushi_x + l) % l
-
-            time_to_removed = entry_time[name] + a_time
-        # 사람 먼저 등장
-        else:
-            a_time = (position[name] - q.x + l) % l
-            time_to_removed = q.t + a_time
-
-        if exit_time[name] < time_to_removed:
-            exit_time[name] = time_to_removed
-
-        queries.append(Query(111, time_to_removed, -1, name, -1))
-
-for name in names:
-    queries.append(Query(222, exit_time[name], -1, name, -1))
-
-queries.sort(key=lambda x: (x.t, x.cmd))
-
-p_num, s_num = 0, 0
-for i in range(len(queries)):
-    if queries[i].cmd == 100:
-        s_num += 1
-    elif queries[i].cmd == 111:
-        s_num -= 1
-    elif queries[i].cmd == 200:
-        p_num += 1
-    elif queries[i].cmd == 222:
-        p_num -= 1
+commands.sort(key=lambda x: (x[1], x[0]))  # 먹고 초밥/손님 제거하는 명령 추가했기 때문에, 시간 순, 명령번호 순 정렬
+for c, t in commands:
+    if c == '100':
+        scount += 1
+    elif c == '111':  # 초밥 제거
+        scount -= 1
+    elif c == '200':
+        ccount += 1
+    elif c == '222':  # 손님 제거
+        ccount -= 1
     else:
-        print(p_num, s_num)
+        print(ccount, scount)
